d86fe92c404e2c3f79153bc1838db6d5baed9112
Delmer Reed <delmer814@yahoo.com>
2014-06-10 02:03:48 -0700

    spec

--- binary_tree.js
+++ binary_tree.js
@@ -4,39 +4,10 @@ function BinTree(value){
   this.value = value;
 };
 
-BinTree.prototype.insert = function(value){
-  if(value < this.value){
-    if(this.left === null){
-      this.left = new BinTree(value);
-    } else {
-      this.left.insert(value)
-    }
-  } else {
-    if(this.right === null){
-      this.right = new BinTree(value)
-    } else {
-      this.right.insert(value);
-    }
-  }
-  return this;
-};
 
-BinTree.prototype.isLeaf = function(){
-  return this.left === null && this.right === null;
-};
+BinTree.prototype.max = function(){
 
-BinTree.prototype.search = function(value){
-  if (this.value == value){
-    return true
-  } else if (this.isLeaf()){
-    return false;
-  } else if (value < this.value){
-    return this.left.search(value);
-  } else {
-    return this.right.search(value);
-  }
-};
-
+}
 try{
   module.exports = BinTree;
 } catch(e){
