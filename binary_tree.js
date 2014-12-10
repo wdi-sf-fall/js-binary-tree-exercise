ed3020322af6f3fa2ee9e3969fa249e10f15d251
Alex Notov <anotov@gmail.com>
2014-12-10 15:23:29 -0800

    Solution

--- binary_tree.js
+++ binary_tree.js
@@ -5,14 +5,42 @@ function BinTree(value){
 }
 
 
-BinTree.prototype.insert = function(){
-
+BinTree.prototype.insert = function(value){
+  console.log("insert", this.value);
+  if (value < this.value) {
+    if (this.left == null) {
+      this.left = new BinTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else if (value > this.value) {
+    if (this.right == null) {
+      this.right = new BinTree(value);  
+    } else {
+      this.right.insert(value);
+    }    
+  }
+  return this;
 };
+
 BinTree.prototype.isLeaf = function(){
-
+  return !this.left && !this.right;
 };
-BinTree.prototype.search = function(){
 
+BinTree.prototype.search = function(value){
+  if (value == this.value) {
+    return true;
+  } else if (this.isLeaf()) {
+    return false;
+  };
+
+  if (value < this.value) {
+    return this.left.search(value);
+  }
+
+  if (value > this.value) {
+    return this.right.search(value);
+  }
 };
 
 
